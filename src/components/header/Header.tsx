f0adb7480bf9473cc54643fa6dfca4f90f61bb88
Gabriel Pires <gabriel.pires@farfetch.com>
2020-09-23 22:12:41 +0100

    feat: Add Header
    This adds the page header with name and description.

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,15 @@
+import { Description, Name, StyledHeader } from './styles';
+import React from 'react';
+
+const Header = () => (
+  <StyledHeader>
+    <Description>Hi! I’m a frontend engineer driven by inovation and 
+      meaningful connections. I focus mainly on improving every 
+      user’s experience with simple yet effective code, <s>even though 
+      most of the time I’m solving bugs I created myself.</s></Description>
+
+    <Name>Gabriel <br/> Pires</Name>
+  </StyledHeader>
+);
+
+export default Header;
