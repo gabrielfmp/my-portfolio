ddaa9bad0241328b88def32c0c91703d0237ed3b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-05 09:14:12 +0000

    Deploy gabrielfmp/my-portfolio to github.com/gabrielfmp/my-portfolio.git:gh-pages

---
+++ precache-manifest.b27142a28f81568917445e32a1b0ec62.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ec8552a81ad65e6e4c1be6cc7632065",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "9b358850e9836b9d3494",
+    "url": "/my-portfolio/static/js/2.a40f8425.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-portfolio/static/js/2.a40f8425.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4173a48ad2a19698d4f2",
+    "url": "/my-portfolio/static/js/main.e7b4ba81.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  },
+  {
+    "revision": "2f8c6e0773edcd63f7b3d2a5977494b8",
+    "url": "/my-portfolio/static/media/cake.2f8c6e07.svg"
+  },
+  {
+    "revision": "b12f724082ec431cab3ae3d55a023d9e",
+    "url": "/my-portfolio/static/media/email.b12f7240.svg"
+  },
+  {
+    "revision": "a911c8633df31bd169e7344be49c5c67",
+    "url": "/my-portfolio/static/media/github.a911c863.svg"
+  },
+  {
+    "revision": "856ef867688090f3fb9133522e313405",
+    "url": "/my-portfolio/static/media/linkedin.856ef867.svg"
+  },
+  {
+    "revision": "63dd9f35c06b1901d422c82834909c6c",
+    "url": "/my-portfolio/static/media/location.63dd9f35.svg"
+  },
+  {
+    "revision": "83afdfbcc2c332b7b898daf040bcd09a",
+    "url": "/my-portfolio/static/media/phone.83afdfbc.svg"
+  }
+]);
